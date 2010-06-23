e1f4f3b5ae2fd54098d506d798d653fc6834b4d6
stormwind <stormwind@gawab.com>
2010-06-23 12:46:35 +0000

    Calculations added

--- tasks/FuzzyPart.java
+++ tasks/FuzzyPart.java
@@ -49,9 +49,9 @@ public class FuzzyPart {
             if (points.get(i)[0] < min) {
                 min = points.get(i)[0];
             }
-            if (points.get(i)[1] < min) {
-                min = points.get(i)[0];
-            }
+//            if (points.get(i)[1] < min) {
+//                min = points.get(i)[0];
+//            }
         }
         return min;
     }
@@ -62,10 +62,49 @@ public class FuzzyPart {
             if (points.get(i)[0] > max) {
                 max = points.get(i)[0];
             }
-            if (points.get(i)[1] > max) {
-                max = points.get(i)[0];
-            }
+//            if (points.get(i)[1] > max) {
+//                max = points.get(i)[0];
+//            }
         }
         return max;
+    }
+
+    public boolean isInPart(float exactValue) {
+        float min = getMinimum();
+        float max = getMaximum();
+        if (exactValue >= getMinimum() && exactValue <= getMaximum()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isTrivial(float exactValue) {
+        for (int i = 0; i < points.size(); i++) {
+            if (exactValue == points.get(i)[0]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public float getTrivialResult(float exactValue) {
+        if (isTrivial(exactValue)) {
+            for (int i = 0; i < points.size(); i++) {
+                if (exactValue == points.get(i)[0]) {
+                    return points.get(i)[1];
+                }
+            }
+        }
+        return -1f;
+    }
+
+    public float getHighestPoint() {
+        for (int i = 0; i < points.size(); i++) {
+            if (points.get(i)[1] == 1) {
+                return points.get(i)[0];
+            }
+        }
+        return -1f;
     }
 }
