920de7b0abcb1956458561ff2a760794feef3b24
stormwind <stormwind@gawab.com>
2010-06-26 12:17:46 +0000

    Created own test files

--- testing/Test4FuzzyResult.java
+++ testing/Test4FuzzyResult.java
@@ -4,6 +4,11 @@
  */
 package testing;
 
+import java.util.ArrayList;
+import java.util.List;
+import tasks.FuzzyPart;
+import tasks.FuzzyResult;
+
 /**
  *
  * @author malygos
@@ -13,7 +18,29 @@ public class Test4FuzzyResult {
     private static Class self = Test4FuzzyResult.class;
 
     public static boolean RunTest() {
-        return false;
+        try {
+            FuzzyResult fr = new FuzzyResult();
+            FuzzyPart part = new FuzzyPart();
+            List<float[]> points = new ArrayList<float[]>();
+            points.add(new float[]{1f,0f});
+            points.add(new float[]{5f,1f});
+            points.add(new float[]{10f,0f});
+            part.setName("part");
+            part.setPoints(points);
+            fr.setPart(part);
+            fr.setExactValue(5f);
+            fr.setEffectiveDegree(1f);
+
+            assert(fr.getPart().equals(part));
+            assert(fr.getExactValue() == 5f);
+            assert(fr.getEffectiveDegree()==1f);
+            
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
--- testing/Test4FuzzyRuleLookupTable.java
+++ testing/Test4FuzzyRuleLookupTable.java
@@ -1,5 +1,11 @@
 package testing;
 
+import java.util.ArrayList;
+import java.util.List;
+import tasks.FuzzyPart;
+import tasks.FuzzyRuleLookupTable;
+import tasks.LinguisticVariable;
+
 /**
  *
  * @author malygos
@@ -9,7 +15,60 @@ public class Test4FuzzyRuleLookupTable {
     private static Class self = Test4FuzzyRuleLookupTable.class;
 
     public static boolean RunTest() {
-        return false;
+        try {
+            FuzzyRuleLookupTable lutab = new FuzzyRuleLookupTable();
+            LinguisticVariable left = new LinguisticVariable();
+            LinguisticVariable top = new LinguisticVariable();
+            left.setName("left");
+            top.setName("top");
+            List<String[]> ruleTable = new ArrayList<String[]>();
+            ruleTable.add(new String[]{"high", "low", "normal"});
+            ruleTable.add(new String[]{"high", "normal", "normal"});
+            ruleTable.add(new String[]{"high", "high", "high"});
+            ruleTable.add(new String[]{"low", "low", "normal"});
+
+            FuzzyPart condition_1 = new FuzzyPart();
+            FuzzyPart condition_2 = new FuzzyPart();
+            FuzzyPart result = new FuzzyPart();
+
+            List<float[]> points = new ArrayList<float[]>();
+            points.add(new float[]{0f, 1f});
+            points.add(new float[]{5f, 0f});
+
+            condition_1.setPoints(points);
+            condition_2.setPoints(points);
+            result.setPoints(points);
+
+            String[] rule = new String[]{"high", "normal", "low"};
+
+            condition_1.setName("cond1");
+            condition_2.setName("cond2");
+            result.setName("result");
+
+            List<FuzzyPart> parts_left, parts_top;
+            parts_left = new ArrayList<FuzzyPart>();
+            parts_top = new ArrayList<FuzzyPart>();
+
+            parts_left.add(condition_1);
+            parts_left.add(condition_2);
+
+            parts_top.add(condition_2);
+            parts_top.add(result);
+
+            left.setFuzzyParts(parts_left);
+            top.setFuzzyParts(parts_top);
+
+            lutab.setLeft(left);
+            lutab.setTop(top);
+            lutab.setRuleSet(ruleTable);
+            lutab.setAndConnection(true);
+
+
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
 
     public static void main(String[] args) {
