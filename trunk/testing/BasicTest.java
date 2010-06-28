6d15048c3fb12659ea8d6fe1cbd738c832b672e6
stormwind@gawab.com <stormwind@gawab.com@45f0b364-e3ad-92b7-29dc-68acc92e62c3>
2010-06-28 21:21:09 +0000

    solved some errors in class "FuzzyPart"

---
+++ trunk/testing/BasicTest.java
@@ -0,0 +1,24 @@
+package testing;
+
+/**
+ *
+ * @author malygos
+ */
+public class BasicTest {
+
+    private static boolean hasOutput;
+  
+    /**
+     * @return the hasOutput
+     */
+    public static boolean isHasOutput() {
+        return hasOutput;
+    }
+
+    /**
+     * @param hasOutput the hasOutput to set
+     */
+    public static void setHasOutput(boolean hasOutput) {
+        hasOutput = hasOutput;
+    }
+}
